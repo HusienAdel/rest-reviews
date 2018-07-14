21bc5da91cfa2a750207224945ee621b4a516145
husien adel <husien@gmail.com>
2018-07-14 12:37:03 +0300

    update

--- js/main.js
+++ js/main.js
@@ -222,7 +222,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 
 // regsiter servervice worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('../sw.js')
+  navigator.serviceWorker.register('/sw.js')
     .then((reg) => {
            console.log('Registration succeeded. Scope is ' + reg.scope);
     }).catch((error) => {
